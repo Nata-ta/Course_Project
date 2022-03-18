008612afb8f699d50d4de590b01076b98708ad46
Nata-ta <natashamatvienko81094@gmail.com>
2022-03-18 15:56:10 +0200

    fixed comments and added likes

--- js/main.js
+++ js/main.js
@@ -1,230 +1,20 @@
-// let postForm = document.querySelector('.allNews');
-// let postContainer = document.querySelector('.posts__body');
-// let loadPostsBtn = postForm.querySelector('._loadPostsBtn');
-
-// let posts = [];
-
-// class Post {
-//     constructor(author, body, img, likes, id, date, comments) {
-
-//         this.author = author;
-//         this.body = body;
-//         this.img = img ? img : '';
-//         this.likes = likes ? likes : 0;
-//         this.id = id ? id : new Date().getTime();
-//         this.date = date ? date : new Date().toLocaleString();
-
-//         this.comments = comments ? comments : 0;
-
-//         this.addLike = this.addLike.bind(this);
-//         this.render();
-//     }
-
-//     addLike() {
-//         this.likes++;
-
-//         const like = document.querySelector(`[data-id="${this.id}"] .posts__likes`);
-//         like.innerHTML = this.likes;
-
-//         posts.forEach(post => {
-//             if (post.id === this.id) {
-//                 post.likes = this.likes;
-//             }
-//         });
-
-//         localStorage.setItem('posts', JSON.stringify(posts));
-//     }
-
-//     render = () => {
-//         let comments = [];
-//         let element = document.createElement('div');
-//         element.dataset.id = this.id;
-//         element.classList.add('posts__news');
-
-//         element.innerHTML = `
-//             <div class="posts__wrap">
-//                 <h3 class="title posts__title">${this.author}</h3>
-//                 <p class="posts__date">${this.date}</p>
-//             </div>
-//             <div class="posts__img-box"></div>
-//             <div class="posts__body-info">
-//                 <div class="posts__descr-body">
-//                     <p class="posts__descr">${this.body}</p>
-//                     <div class="posts__descr-item">
-//                         <button class="posts__likes-btn"><span class="posts__likes">${this.likes}</span></button>
-//                         <button class="posts__comm-btn"><span class="posts__comm"></span></button>
-//                     </div>
-//                 </div>
-//             </div>
-//         `;
-
-//         if (this.img) {
-//             let imgBox = element.querySelector('.posts__img-box');
-//             imgBox.style.width = '100%';
-//             imgBox.style.margin = '0 0 30px';
-//             let image = document.createElement('img');
-//             image.src = `${this.img}`;
-//             image.classList.add('posts__img');
-
-//             imgBox.appendChild(image);
-//         }
-
-//         let likeBtn = element.querySelector('.posts__likes-btn');
-//         likeBtn.addEventListener('click', this.addLike);
-
-//         let block = document.createElement('div');
-//         block.classList.add('posts__comment');
-
-//         block.innerHTML = `
-//             <form class="commentSubmit">
-//                 <h3 class="title posts__comment-title">Comments</h3>
-//                 <div class="posts__form-body">
-
-
-//                 </div>
-//                 <div class="posts__authorForm">
-//                     <label for="nameAuthor" class="posts__form-author">Author</label>
-//                     <input type="text" class="posts__form-authorControl" id="nameAuthor" name="authorName">
-//                 </div>
-//                 <div class="posts__comment-textForm">
-//                     <label for="commentText" class="posts__comment-label">New comment</label>
-//                     <textarea class="posts__comment-text" id="commentText" rows="3" name="commText"></textarea>
-//                 </div>
-//                 <button class="btn btn-submit posts__comment-btn" type="submit">Send post</button>
-//             </form>
-//         `;
-
-//         element.appendChild(block);
-
-//         let commentBtn = element.querySelector('.posts__comm-btn');
-//         commentBtn.addEventListener('click', function (e) {
-//             block.classList.toggle('posts__comment-show');
-
-//         });
-
-//         let commentContainer = element.querySelector('.posts__form-body');
-
-//         let commentSubmit = element.querySelector('.commentSubmit');
-
-//         const onFormCommentSubmit = (e) => {
-//             e.preventDefault();
-
-//             let userName = commentSubmit.authorName.value;
-//             let commentText = commentSubmit.commText.value;
-//             let commentDate = new Date().toLocaleString();
-
-//             let commentsData = {
-//                 userName,
-//                 commentDate,
-//                 commentText
-//             };
-
-//             comments.push(commentsData);
-
-//             let item = document.createElement('div');
-//             item.dataset.id = this.id;
-//             item.classList.add('posts__news');
-
-//             comments.forEach(comment => {
-//                 item.innerHTML = `
-//                     <div class="posts__comment-body">
-//                         <div class="posts__comment-wrap">
-//                             <h4 class="posts__comment-author">${comment.userName}</h4>
-//                             <p class="posts__comment-date">${comment.commentDate}</p>
-//                         </div>
-//                         <p class="posts__comment-descr">${comment.commentText}</p>
-//                     </div>
-//                 `;
-//             })
-
-//             this.comments = comments;
-
-//             posts.forEach(post => {
-//                 if (post.id === this.id) {
-//                     post.comments = this.comments;
-//                 }
-//             });
-
-//             localStorage.setItem('posts', JSON.stringify(posts));
-
-//             commentContainer.appendChild(item);
-
-//             commentSubmit.reset();
-//         }
-
-//         commentSubmit.addEventListener('submit', onFormCommentSubmit);
-
-//         postContainer.appendChild(element);
-//     }
-// }
-
-// const onFormSubmit = (e) => {
-//     e.preventDefault();
-
-//     let newPost = new Post(e.target.author.value, e.target.text.value, e.target.image.value);
-
-//     posts.unshift(newPost);
-
-//     let jsonPost = JSON.stringify(posts);
-
-//     localStorage.setItem('posts', jsonPost);
-
-//     postForm.reset();
-// }
-
-// postForm.addEventListener('submit', onFormSubmit);
-
-// const GetSavedPosts = () => {
-//     let data = localStorage.getItem('posts');
-
-//     if (data !== null) {
-//         posts = JSON.parse(data);
-//         return posts;
-//     }
-
-//     return null;
-// }
-
-// let postsFromStorage = GetSavedPosts();
-
-// if (postsFromStorage !== null) {
-//     postsFromStorage.forEach(function (post) {
-//         new Post(post.author, post.body, post.img, post.likes, post.id, post.date, post.comments);
-//     })
-// }
-
 let postForm = document.querySelector('.allNews');
 let postContainer = document.querySelector('.posts__body');
 let loadPostsBtn = postForm.querySelector('._loadPostsBtn');
 
 let url = 'http://localhost:3000/posts';
 
-const addLike = (element) => (e) => {
-    console.log(element, e);
-
-    let likes = 0;
-    likes++;
-    // const like = document.querySelector(`[data-id="${element.id}"] .posts__likes`);
-    // like.innerHTML = counter;
-
-    fetch(`${url}/${element.dataset.id}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-}
-
 const renderComment = (data) => {
-    let commentContainer = document.querySelector(`[data-id="${data.id}"] .posts__form-body`);
-    let item = document.createElement('div');
-    item.dataset.id = data.id;
-    item.classList.add('posts__news');
 
     let allComments = data.comments;
-    console.log(allComments);
 
     allComments.forEach(comment => {
+        let commentContainer = document.querySelector(`[data-id="${data.id}"] .posts__form-body`);
+
+        let item = document.createElement('div');
+        item.dataset.id = data.id;
+        item.classList.add('posts__news');
+
         item.innerHTML = `
             <div class="posts__comment-body">
                 <div class="posts__comment-wrap">
@@ -236,10 +26,22 @@ const renderComment = (data) => {
         `;
 
         commentContainer.appendChild(item);
+
+        return;
     });
 };
 
 const renderePost = (data) => {
+    let comments = [];
+
+    if (data.comments !== null) {
+        data.comments.forEach(comment => {
+            comments.push(comment);
+
+            return;
+        });
+    };
+
     let element = document.createElement('div');
     element.dataset.id = data.id;
     element.classList.add('posts__news');
@@ -254,8 +56,8 @@ const renderePost = (data) => {
             <div class="posts__descr-body">
                 <p class="posts__descr">${data.body}</p>
                 <div class="posts__descr-item">
-                    <button class="posts__likes-btn"><span class="posts__likes"></span></button>
-                    <button class="posts__comm-btn"><span class="posts__comm"></span></button>
+                    <button class="posts__likes-btn"><span class="posts__likes">${data.likes}</span></button>
+                    <button class="posts__comm-btn"><span class="posts__comm">${comments.length}</span></button>
                 </div>
             </div>
         </div>
@@ -273,7 +75,25 @@ const renderePost = (data) => {
     };
 
     let likeBtn = element.querySelector('.posts__likes-btn');
-    likeBtn.addEventListener('click', addLike(element));
+    likeBtn.addEventListener('click', function (e) {
+
+        data.likes++;
+
+        const like = document.querySelector(`[data-id="${data.id}"] .posts__likes`);
+        like.innerHTML = `${data.likes}`;
+
+        let allLikes = data.likes;
+
+        fetch(`${url}/${data.id}`, {
+            method: "PATCH",
+            body: JSON.stringify({
+                likes: allLikes
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    });
 
     postContainer.appendChild(element);
 
@@ -301,14 +121,11 @@ const renderePost = (data) => {
     let commentBtn = element.querySelector('.posts__comm-btn');
     commentBtn.addEventListener('click', function (e) {
         block.classList.toggle('posts__comment-show');
-
     });
 
     let commentContainer = element.querySelector('.posts__form-body');
 
     let commentSubmit = element.querySelector('.commentSubmit');
-
-    let comments = [];
 
     const onFormCommentSubmit = (e) => {
         e.preventDefault();
@@ -322,8 +139,6 @@ const renderePost = (data) => {
             commentDate,
             commentText
         };
-
-        comments.push(commentsData);
 
         let item = document.createElement('div');
         item.dataset.id = data.id;
@@ -339,9 +154,9 @@ const renderePost = (data) => {
             </div>
         `;
 
-        data.comments = comments;
+        commentContainer.appendChild(item);
 
-        console.log(comments);
+        comments.push(commentsData);
 
         fetch(`${url}/${data.id}`, {
             method: "PATCH",
@@ -353,7 +168,8 @@ const renderePost = (data) => {
             }
         });
 
-        commentContainer.appendChild(item);
+        let postCommCounter = element.querySelector('.posts__comm');
+        postCommCounter.innerHTML = `${comments.length}`;
 
         commentSubmit.reset();
     };
@@ -368,7 +184,8 @@ postForm.addEventListener('submit', function () {
         img: postForm.image.value,
         likes: 0,
         id: new Date().getTime(),
-        date: new Date().toLocaleString()
+        date: new Date().toLocaleString(),
+        comments: []
     };
 
     let jsonData = JSON.stringify(data);
@@ -388,7 +205,9 @@ fetch(url)
     .then(res => res.json())
     .then(res => {
         res.forEach(item => {
-            renderePost(item);
-            renderComment(item);
+            if (item !== null) {
+                renderePost(item);
+                renderComment(item);
+            }
         })
     });
